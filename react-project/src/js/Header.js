9665c1a07e0a42cadd25936e1a13f4aa4694d47a
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2022-03-31 12:18:21 +0200

    Add logo
    #4

--- react-project/src/js/Header.js
+++ react-project/src/js/Header.js
@@ -5,6 +5,7 @@ import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import logo from '../img/logo.png';
 
 function Header() {
     return (
@@ -12,7 +13,7 @@ function Header() {
             <Link to='/'>
                 <img
                     className="header__icon"
-                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    src={logo}
                     alt=""
                 />
             </Link>
