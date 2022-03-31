4484824f2f18c280126e3c8c90912322b3a3b851
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2022-03-31 12:08:08 +0200

    Change title in Home.js
    #4

--- react-project/src/js/Home.js
+++ react-project/src/js/Home.js
@@ -13,8 +13,8 @@ function Home() {
             <div className='home__section'>
             <Card
                 src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-                title="Online Experiences"
-                description="Unique activities we can do together, led by a world of hosts."
+                title="Accessibility for all"
+                description="Find a place that suits you !"
             />
             <Card
                 src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
